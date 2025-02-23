678a8fe9db5d04a265450528d0a56a5abed54238
KAICHA-KL <kaicha.reality@gmail.com>
2025-02-23 14:06:58 +0530

    Add javascript boilerplate framework and testing
    Add input functionality and getting stored in the
    constructor

--- src/script.js
+++ src/script.js
@@ -1,12 +1,31 @@
 const mylibrary = [];
 
-function book(title,genre,author,pages){
+function book(title,genre,pages,readStatus){
  this.bookTitle = title;
  this.bookGenre = genre;
- this.bookAuthor = author;
  this.bookPages = pages;
+ this.bookReadStatus = readStatus
 }
+
+// src/script.js
+console.log("Script loaded successfully!"); 
 
 function addBookToLibrary() { 
 
 }
+
+const addBookBtn = document.querySelector('.submitBtn');
+const inputPages = document.getElementById('formPages');
+const inputTitle = document.getElementById('formTitle');
+const inputGenre = document.getElementById('formGenre');
+const inputReadStatus = document.getElementById('formReadStatus');
+const modal = document.getElementById('my_modal_1');
+
+addBookBtn.addEventListener('click',(e)=>{
+
+    e.preventDefault();
+    let newbook = new book(inputTitle.value,inputGenre.value,inputPages.value,inputReadStatus.checked);
+    alert(newbook.bookReadStatus);
+
+    modal.close();
+});
