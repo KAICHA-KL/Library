b48d65945d1813235a16a80290dcb172b767898e
KAICHA-KL <kaicha.reality@gmail.com>
2025-02-23 17:26:21 +0530

    Basic add functionality without some elements

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 const mylibrary = [];
+let newbook ={};
 
+//book constructor//
 function book(title,genre,pages,readStatus){
  this.bookTitle = title;
  this.bookGenre = genre;
@@ -7,14 +9,58 @@ function book(title,genre,pages,readStatus){
  this.bookReadStatus = readStatus
 }
 
-// src/script.js
-console.log("Script loaded successfully!"); 
-
 function addBookToLibrary() { 
+mylibrary.push(newbook);
+}
+
+function createCard(item) {
+
+    //Create card container 
+
+  const card = document.createElement('div');
+  card.className = 'card bg-base-100 shadow-xl min-w-fit';
+
+  const cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.className = 'card-title';
+  cardTitle.textContent = item.bookTitle;
+
+  cardBody.append(cardTitle);
+  card.append(cardBody);
+
+  return card;
 
 }
 
+function addBooks() {
+
+    const container = document.getElementById('card-container');
+    const len = mylibrary.length;
+    container.appendChild(createCard(mylibrary[len -1]));
+    }
+
+function displayBooks() {
+
+const container = document.getElementById('card-container');
+const fragment = document.createDocumentFragment();
+
+mylibrary.forEach(item =>{
+    fragment.appendChild(createCard(item));
+});
+
+container.appendChild(fragment);
+}
+
+function clearBooks(){
+    const container = document.getElementById('toDelete');
+    container.remove();
+}
+
+
 const addBookBtn = document.querySelector('.submitBtn');
+const removeBookBtn = document.querySelector('.removeBtn')
 const inputPages = document.getElementById('formPages');
 const inputTitle = document.getElementById('formTitle');
 const inputGenre = document.getElementById('formGenre');
@@ -24,8 +70,17 @@ const modal = document.getElementById('my_modal_1');
 addBookBtn.addEventListener('click',(e)=>{
 
     e.preventDefault();
-    let newbook = new book(inputTitle.value,inputGenre.value,inputPages.value,inputReadStatus.checked);
-    alert(newbook.bookReadStatus);
-
+    newbook = new book(inputTitle.value,inputGenre.value,inputPages.value,inputReadStatus.checked);
+    addBookToLibrary();
     modal.close();
+    //clearBooks();
+    addBooks();
+
 });
+
+removeBookBtn.addEventListener('click',(e)=>{
+    
+})
+
+
+displayBooks();
